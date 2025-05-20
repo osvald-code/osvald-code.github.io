fe3eb8544e21a7f26607e7e7df6664c46208a1a5
ulty <47011082+ulty-o@users.noreply.github.com>
2025-05-19 23:04:50 -0400

    pagination
    basic pagination added to blog page

--- src/app/blog/BlogClient.tsx
+++ src/app/blog/BlogClient.tsx
@@ -12,6 +12,16 @@ import DecryptedText from "@/components/ui/DecryptedText";
 import CustomFooter from "@/components/customfooter";
 import { Button } from "@/components/ui/button";
 import { posts } from "../mdx-page/posts";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+ 
 
 export default function BlogClient() {
   const [mounted, setMounted] = useState(false);
@@ -54,18 +64,44 @@ export default function BlogClient() {
       <Separator orientation="horizontal" className="min-h-[1px] mx-[2%] border-[1px] max-w-[95%]"/>
       <Card className="mx-2 sm:mx-20 my-4 items-center lg:shadow-xl shadow-black/50 justify-center dark:bg-slate-800/10">
         <h1 className="text-4xl font-bold">blog</h1>
+        <Pagination className="mt-4 w-[100%]">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious  
+                onClick={() => {
+                  const nextIndex = Number((postIndex - 1) > 0) ;
+                  setPostName(nextIndex);
+                }} />
+            </PaginationItem>
+            {posts.map((post, idx) =>
+                          ( 
+                            <PaginationItem key={post.name}>
+                              <PaginationLink 
+                                isActive={postIndex===idx} 
+                                onClick={() => {
+                                  setPostName(idx);
+                                }}> 
+                                {idx + 1}
+                              </PaginationLink>
+                              {/* <PaginationLink isActive={postIndex===idx}  href={"blog/?postName=" + post.name}> {idx + 1}</PaginationLink> */}
+                            </PaginationItem>
+                          )
+                        )}
+            <PaginationItem> <PaginationEllipsis /></PaginationItem>
+            <PaginationItem>
+              <PaginationNext  
+                onClick={() => {
+                const nextIndex = (postIndex + 1) % posts.length;
+                setPostName(nextIndex);
+              }}/>
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+        
         <p className="text-lg mt-4">{posts[postIndex].name}</p>
         <ScrollArea className="prose h-[80%] w-[100%] p-6  prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-headings:text-white">
           <Post postName={posts[postIndex].name}/>
         </ScrollArea>
-        <Button
-          onClick={() => {
-            const nextIndex = (postIndex + 1) % posts.length;
-            setPostName(nextIndex);
-          }}
-        >
-          Next Post
-        </Button>
       </Card>
       <CustomFooter/>
     </main>
