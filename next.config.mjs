1b1c8332fe11425741615e9ee3c4b670ea8b4525
ulty <47011082+ulty-o@users.noreply.github.com>
2025-05-20 01:13:38 -0400

    mdx table config
    checking if error is related to turbopack, as build is sucessful

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+// next.config.mjs  (or .ts)  – Pure ESM, no CommonJS
+import createMDX   from '@next/mdx'
+import remarkGfm   from 'remark-gfm'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js','jsx','ts','tsx','md','mdx'],
+  experimental: { mdxRs: false },   // ← JS compiler
+}
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],     // must be a *function*, not undefined
+    rehypePlugins: [],
+  },
+})
+
+export default withMDX(nextConfig)
+
--- src/app/mdx-page/posts.ts
+++ src/app/mdx-page/posts.ts
@@ -1,5 +1,6 @@
 
 export const posts = [
+  { name: "acnl_fish", date:"2025-19-05", component: () => import('./acnl_fish_guide.mdx') },
   { name: "b1", date:"2025-17-05", component: () => import('./b1.mdx') },
   { name: "b2", date:"2025-17-05", component: () => import('./b2.mdx') },
   { name: "b3", date:"2025-17-05", component: () => import('./b3.mdx') },
