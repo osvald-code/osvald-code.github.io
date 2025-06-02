501da212cfc56d2312a07b4a1bb6ac64ea964d4e
osvald-code <47011082+osvald-code@users.noreply.github.com>
2025-06-02 12:07:49 -0400

    Update page.tsx
    added min with to main card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
     </div>
       <Separator orientation="horizontal" className="min-h-[1px] mx-[2%] border-[1px] max-w-[95%]"/>
       
-      <Card className="w-[90%] mx-auto sm:mx-auto my-35 sm:my-5 py-30 sm:py-2 sm:px-15 sm:w-fit flex flex-col items-center lg:shadow-xl shadow-black/50 justify-center min-h-[100%] lg:min-h-[calc(90vh-4rem)] dark:bg-zinc-950/50">
+      <Card className="w-[90%] mx-auto sm:mx-auto my-35 sm:my-5 py-30 sm:py-2 sm:px-15 sm:w-fit sm:min-w-[80%] flex flex-col items-center lg:shadow-xl shadow-black/50 justify-center min-h-[100%] lg:min-h-[calc(90vh-4rem)] dark:bg-zinc-950/50">
         <div className="flex flex-col items-center justify-center">
           {/* there is sort of a string con-cat happening here -> text is '[osvald::dev]' */}
           <p className="hidden sm:block text-3xl mr-auto px-30">
